52124ba94f3068ca60d194f4debd7e6667e56efd
mkN <mknord1565@gmail.com>
2022-06-26 14:33:39 +0300

    add скачивание_изображения.py

--- requests_module/скачивание_изображения.py
+++ requests_module/скачивание_изображения.py
@@ -18,10 +18,3 @@ with open(picture_from_web_2, 'wb') as file:
             print('chunk', counter)
             counter += 1
             file.write(chunk)
-
-web_link_3 = r'https://yandex.ru'
-saved_html = r'new_file_html.html'
-response_3 = requests.get(web_link_3)
-############################### скачиваем html страницу
-with open(saved_html, 'wb') as file:
-    file.write(response_3.content)
