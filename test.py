40667801a21cebd55df05f45bfdc53be8316739f
mkN <mknord1565@gmail.com>
2022-07-09 22:41:17 +0300

    test.py

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-try:
-    num = int(input("enter a number: "))
-    print(f"you've entered {num}")
-    qweqwe
-except ValueError:
-    print("you've entered not a number")
+def function_1():
+    print("hello")
+
+def function_2():
+    print("bye")
+
+funcs = [function_1, function_2]
+funcs[0]()
+funcs[1]()
+
+# Будет работать и выводить
+# hello
+# bye
