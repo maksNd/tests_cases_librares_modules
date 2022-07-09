46a812300b376560f222c92f17cffd2840b476db
mkN <mknord1565@gmail.com>
2022-07-10 01:37:20 +0300

    improvements datetime_module.py

--- datetime_module/datetime_module.py
+++ datetime_module/datetime_module.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 
 now = datetime.datetime.now()  # текущая дата до микросекунд
 print(now)
@@ -11,3 +12,8 @@ print()
 
 print(now.strftime("%Y-%m-%d %H:%M"))  # формирование даты в любой формат
 print(now.strftime("%X"))  # текущее время без микросекунд
+
+start = datetime.datetime.now()
+time.sleep(1)
+end = datetime.datetime.now()
+print(end - start)  # разница между двумя датами
