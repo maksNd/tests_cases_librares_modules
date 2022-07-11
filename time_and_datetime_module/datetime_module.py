56b14c80ccb967492c66b4b61c3c7f8e9845badf
mkN <mknord1565@gmail.com>
2022-07-11 20:52:07 +0300

    continue learning datetime_module

--- time_and_datetime_module/datetime_module.py
+++ time_and_datetime_module/datetime_module.py
@@ -6,6 +6,7 @@ print(now)
 
 print()
 
+"""Так можно получитьтекущий год, месяц, день, час и т.д."""
 print(f"{now.year}-{now.month}-{now.day} {now.hour}:{now.minute}:{now.second}.{now.microsecond}")
 
 print()
@@ -17,3 +18,10 @@ start = datetime.datetime.now()
 time.sleep(1)
 end = datetime.datetime.now()
 print(end - start)  # разница между двумя датами
+print()
+
+"""Конвертировать Unix timestamp в обычный формат даты"""
+ts = int("1284101485")
+# if you encounter a "year is out of range" error the timestamp
+# may be in milliseconds, try 'ts /= 1000' in that case
+print(datetime.datetime.utcfromtimestamp(ts).strftime('%d-%m-%Y %H:%M:%S'))
