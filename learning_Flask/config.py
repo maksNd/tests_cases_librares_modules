1fc1137d5ebf70540f83fe98569d30ce17a0cdcb
mkN <mknord1565@gmail.com>
2022-07-30 10:58:20 +0300

    learning_lask

---
+++ learning_Flask/config.py
@@ -0,0 +1,16 @@
+"""
+Что можно хранить в файле с конфигурациями?
+
+Настройки дебага и приложения, например DEBUG = True
+Адреса внешних ресурсов, например http://data-source.com/data.json
+Названия файлов с данными, например, data.json
+Пароли к внешним сервисам, например SKYPROPASS = "12345"
+"""
+
+TESTING = True
+DEBUG = True
+FLASK_ENV = 'development'
+SECRET_KEY = 'GDtfDCFYjD'
+
+PROJECT_NAME = "Все о Flask"
+PROJECT_DESCRIPTION = "Рецепты, руководства и примеры по Flask"
---
+++ learning_Flask/example_flask_with_config_from_file.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+app.config.from_pyfile('config.py')  # чтобы все конфиги применились к приложению,
+                                     # вызываем такой метод с указанием файла
+
+# print(app.config)
+
+@app.route('/')
+def index_page():
+    title = app.config.get('PROJECT_NAME')
+    description = app.config.get('PROJECT_DESCRIPTION')
+
+    return f'<h1>{title}</h1><p>{description}</p>'
+
+app.run()
